693e7048d85e9c0b1054618101e70d909b51143c
hocinebouarara <hocinbouarara08@gmail.com>
2020-07-21 23:21:24 +0100

    create class contain links of view and css files

---
+++ src/helpers/Links.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * @author hocin
+ */
+public class Links {
+
+    public static String PRODUCTSVIEW = "/products/productsView.fxml";
+    public static String PRODUCTSCSS = "/products/productsview.css";
+
+    public static String CLIENTSVIEW = "/clients/clientsView.fxml";
+    public static String CLIENTSCSS = "/clients/clientsview.css";
+
+}
