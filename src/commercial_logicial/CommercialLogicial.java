c027dfdcffa592848117d9902f68b0de8512369e
hocinebouarara <hocinbouarara08@gmail.com>
2020-07-21 19:49:22 +0100

    modify class main for show view product

--- src/commercial_logicial/CommercialLogicial.java
+++ src/commercial_logicial/CommercialLogicial.java
@@ -5,13 +5,17 @@
  */
 package commercial_logicial;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,23 +24,11 @@ import javafx.stage.Stage;
 public class CommercialLogicial extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/products/productsView.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(CommercialLogicial.class.getResource("/products/productsview.css").toExternalForm());
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
